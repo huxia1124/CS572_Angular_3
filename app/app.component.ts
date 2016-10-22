e5f52d641c32955288881604ce5ea697ae2e708a
Huan Xia <huxia1124@gmail.com>
2016-10-21 21:55:35 -0500

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,8 +25,7 @@ import { OnInit }         from '@angular/core'
     background-color: #CFD8DC !important;
     color: white;
   }
-  .products {
-    margin: 0 0 2em 0;
+  .products {    margin: 0 0 2em 0;
     list-style-type: none;
     padding: 0;
     width: 15em;
@@ -74,7 +73,7 @@ import { OnInit }         from '@angular/core'
 })
 
 export class AppComponent implements OnInit {
-  title = "CS472 Angular / Lab 2";
+  title = "CS572 Angular / Lab 2";
   selectedProduct : Product;
   products : Product[];
 
